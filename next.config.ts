90d9e1fb4579812747689baf9f320ab9d4d327e3
Komron-Nazarov <komron7nazarov@gmail.com>
2026-05-28 20:40:08 +0500

    Status

--- next.config.ts
+++ next.config.ts
@@ -11,21 +11,42 @@
 // export default nextConfig;
 
 
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   // images: {
+//   //   domains: ["your-project.supabase.co"],
+//   // },
+
+//   images: {
+//   remotePatterns: [
+//     {
+//       protocol: "https",
+//       hostname: "ylcrmbfuyuvmnupedoks.supabase.co",
+//     },
+//   ],
+// }
+// };
+
+// export default nextConfig;
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // images: {
-  //   domains: ["your-project.supabase.co"],
-  // },
-
   images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "ylcrmbfuyuvmnupedoks.supabase.co",
-    },
-  ],
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Две звёздочки означают ВООБЩЕ ЛЮБОЙ домен в интернете
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
